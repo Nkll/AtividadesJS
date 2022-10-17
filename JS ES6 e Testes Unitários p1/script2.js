4847679b884994dee4d5b6f4faeecdcb9611cc07
Nkll <nikollas.coradini@gmail.com>
2022-10-17 18:45:45 -0300

    JS ES6 arrow functions e template literals

---
+++ JS ES6 e Testes Unitários p1/script2.js
@@ -0,0 +1,8 @@
+const oddsAndEvens = [13, 3, 4, 10, 7, 2];
+
+const sortOddsAndEvens = () => {
+  oddsAndEvens.sort((a, b) => {return a-b});
+  console.log(`Os números ${oddsAndEvens} se encontram ordenados de forma crescente!`);
+}
+
+sortOddsAndEvens();
